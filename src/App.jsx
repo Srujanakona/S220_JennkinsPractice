a7bc99823785dfaf5c22e06f19b72fe44fb9f1a8
Srujanakona <162702559+Srujanakona@users.noreply.github.com>
2025-08-22 22:03:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecomJenkinsPractice">
+    <BrowserRouter basename="/ecomJennkinsPractice">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
